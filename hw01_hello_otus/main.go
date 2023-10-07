9e9d31eba0f5051a69b6a363843d0b2de2fc10ea
Владислав <gardashvs@comlogic.ru>
2023-10-07 21:32:45 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
-	fmt.Println(reverse.String("Hello, OTUS!"))
+	println(reverse.String("Hello, OTUS!"))
 }
