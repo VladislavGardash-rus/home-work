ae199efc919ee965b77f9d5ffd502837ab5f6415
Владислав <gardashvs@comlogic.ru>
2023-11-11 20:30:46 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,6 +3,7 @@ package hw05parallelexecution
 import (
 	"errors"
 	"sync"
+	"time"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -12,28 +13,48 @@ type Task func() error
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, maxProcessCount, maxErrorCount int) error {
 	taskCh := make(chan Task)
-	defer close(taskCh)
+	closeCh := make(chan struct{})
+
+	defer func() {
+		close(taskCh)
+		close(closeCh)
+	}()
 
 	errorCount := 0
+
 	wg := new(sync.WaitGroup)
 	wg.Add(len(tasks))
+
 	mtx := new(sync.Mutex)
+
 	for i := 0; i < maxProcessCount; i++ {
 		go func(taskCh chan Task, wg *sync.WaitGroup, mtx *sync.Mutex, errorCount *int) {
-			for task := range taskCh {
-				err := task()
-				if err != nil {
-					mtx.Lock()
-					*errorCount++
-					mtx.Unlock()
+			for {
+				select {
+				case task := <-taskCh:
+					if task != nil {
+						err := task()
+						mtx.Lock()
+						if err != nil {
+							*errorCount++
+						}
+						mtx.Unlock()
+						wg.Done()
+					}
+				case <-closeCh:
+					return
 				}
-				wg.Done()
 			}
 		}(taskCh, wg, mtx, &errorCount)
 	}
 
 	for i := range tasks {
-		if errorCount == maxErrorCount {
+		if errorCount >= maxErrorCount {
+			for i := 0; i < maxProcessCount; i++ {
+				closeCh <- struct{}{}
+			}
+
+			time.Sleep(500 * time.Millisecond)
 			return ErrErrorsLimitExceeded
 		}
 
