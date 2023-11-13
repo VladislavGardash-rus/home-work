b87b5872372424c5aa0aae7ad4c049794a80e215
Владислав <gardashvs@comlogic.ru>
2023-11-14 01:14:53 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,65 +3,58 @@ package hw05parallelexecution
 import (
 	"errors"
 	"sync"
-	"time"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type TaskResult struct {
+	Err error
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, maxProcessCount, maxErrorCount int) error {
-	taskCh := make(chan Task)
-	closeCh := make(chan struct{})
+	if maxErrorCount <= 0 {
+		return ErrErrorsLimitExceeded
+	}
 
-	defer func() {
-		close(taskCh)
-		close(closeCh)
+	taskCh := make(chan Task, len(tasks))
+
+	func() {
+		defer close(taskCh)
+		for i := range tasks {
+			taskCh <- tasks[i]
+		}
 	}()
 
 	errorCount := 0
+	var mainErr error
 
 	wg := new(sync.WaitGroup)
-	wg.Add(len(tasks))
-
 	mtx := new(sync.Mutex)
 
 	for i := 0; i < maxProcessCount; i++ {
-		go func(taskCh chan Task, wg *sync.WaitGroup, mtx *sync.Mutex, errorCount *int) {
-			for {
-				select {
-				case task := <-taskCh:
-					if task != nil {
-						err := task()
-						mtx.Lock()
-						if err != nil {
-							*errorCount++
-						}
-						mtx.Unlock()
-						wg.Done()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for task := range taskCh {
+				err := task()
+				if err != nil {
+					mtx.Lock()
+					errorCount++
+					mtx.Unlock()
+					if errorCount >= maxErrorCount {
+						mainErr = ErrErrorsLimitExceeded
+						return
 					}
-				case <-closeCh:
-					return
 				}
 			}
-		}(taskCh, wg, mtx, &errorCount)
-	}
-
-	for i := range tasks {
-		if errorCount >= maxErrorCount {
-			for i := 0; i < maxProcessCount; i++ {
-				closeCh <- struct{}{}
-			}
-
-			time.Sleep(500 * time.Millisecond)
-			return ErrErrorsLimitExceeded
-		}
-
-		taskCh <- tasks[i]
+		}()
 	}
 
 	wg.Wait()
 
-	return nil
+	return mainErr
 }
