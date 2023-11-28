ddbc84b5fc7d85bce72c61f7855d178b877c3ca2
Владислав <gardashvs@comlogic.ru>
2023-11-29 01:32:59 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,46 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(Bi)
+
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case value := <-in:
+				if value == nil {
+					wg.Done()
+					return
+				}
+
+				wg.Add(1)
+
+				for _, stage := range stages {
+					newIn := make(Bi)
+					go func() {
+						newIn <- value
+					}()
+
+					value = <-stage(newIn)
+
+					close(newIn)
+				}
+
+				out <- value
+				wg.Done()
+
+			case <-done:
+				close(out)
+				return
+			}
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
