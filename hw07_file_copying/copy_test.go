f932df9a50b0d1814499c11db58a6ec55ec83117
Владислав <gardashvs@comlogic.ru>
2023-12-04 22:11:41 +0300

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,95 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const sourceFileName = "testdata/input.txt"
+const targetFileName = "out.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("negative offset", func(t *testing.T) {
+		err := Copy(sourceFileName, targetFileName, -1, 0)
+		require.Equal(t, nil, err)
+
+		resourceFileInfo, _ := os.Stat(sourceFileName)
+		targetFileInfo, _ := os.Stat(targetFileName)
+		require.Equal(t, resourceFileInfo.Size(), targetFileInfo.Size())
+
+		err = remove(targetFileName)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("negative limit", func(t *testing.T) {
+		err := Copy(sourceFileName, targetFileName, 0, -1)
+		require.Equal(t, nil, err)
+
+		resourceFileInfo, _ := os.Stat(sourceFileName)
+		targetFileInfo, _ := os.Stat(targetFileName)
+		require.Equal(t, resourceFileInfo.Size(), targetFileInfo.Size())
+
+		err = remove(targetFileName)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("negative limit offset", func(t *testing.T) {
+		err := Copy(sourceFileName, targetFileName, -1, -1)
+		require.Equal(t, nil, err)
+
+		resourceFileInfo, _ := os.Stat(sourceFileName)
+		targetFileInfo, _ := os.Stat(targetFileName)
+		require.Equal(t, resourceFileInfo.Size(), targetFileInfo.Size())
+
+		err = remove(targetFileName)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("without source file", func(t *testing.T) {
+		err := Copy("", targetFileName, 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("limit 100", func(t *testing.T) {
+		err := Copy(sourceFileName, targetFileName, 0, 100)
+		require.Equal(t, nil, err)
+
+		targetFileInfo, _ := os.Stat(targetFileName)
+		require.Equal(t, int64(100), targetFileInfo.Size())
+
+		err = remove(targetFileName)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("offset 50", func(t *testing.T) {
+		err := Copy(sourceFileName, targetFileName, 50, 4)
+		require.Equal(t, nil, err)
+
+		targetFileInfo, err := os.Stat(targetFileName)
+		require.Equal(t, nil, err)
+		require.Equal(t, int64(4), targetFileInfo.Size())
+
+		file, err := os.Open(targetFileName)
+		require.Equal(t, nil, err)
+
+		data, err := io.ReadAll(file)
+		require.Equal(t, nil, err)
+
+		require.Equal(t, "Play", string(data))
+
+		file.Close()
+		err = remove(targetFileName)
+		require.Equal(t, nil, err)
+	})
+}
+
+func remove(name string) error {
+	err := os.Remove(name)
+	if err != nil {
+		return err
+	}
+	return nil
 }
